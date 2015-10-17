ba91c9e2aaa29f56ca193dff141e478679378118
dervism <dervis4@gmail.com>
2015-10-17 13:22:20 +0200

    File reading and string manipulation.

---
+++ src/file_read.erl
@@ -0,0 +1,16 @@
+-module(file_read).
+
+%% API
+-export([readfile/1]).
+
+readfile(FileName) ->
+  {ok, Device} = file:open(FileName, [read]),
+  try get_all_lines(Device)
+  after file:close(Device)
+  end.
+
+get_all_lines(Device) ->
+  case io:get_line(Device, "") of
+    eof  -> [];
+    Line -> Line ++ get_all_lines(Device)
+  end.
--- src/temprature_sensorhandler.erl
+++ src/temprature_sensorhandler.erl
@@ -21,20 +21,28 @@ content_types_provided(Req, State) ->
   ], Req, State}.
 
 html(Req, State) ->
-  Temprature = read_temp(),
+  Temprature = read_temperature(),
   {ok, Body} = temprature_dtl:render([{t, Temprature}]),
   {ok, Req2} = cowboy_req:reply(200, [{<<"content-type">>, <<"text/html">>}], Body, Req),
   {ok, Req2, State}.
 
 json(Req, State) ->
-  Temprature = read_temp(),
+  Temprature = read_temperature(),
   Body = io:format("{\"t\": \"~p\"}", Temprature),
   {Body, Req, State}.
 
 text(Req, State) ->
-  Temprature = read_temp(),
+  Temprature = read_temperature(),
   Text = io:format("Temprature: ~p", Temprature),
   {<<Text>>, Req, State}.
 
-read_temp() ->
-  23.5.
+read_temperature() ->
+  DS18B20_SensorOutput = read_file(),
+  Line = lists:last(DS18B20_SensorOutput),
+  Value = lists:last(re:split(Line, "t=", [{return,list},trim])),
+  Temperatur = string:to_integer(Value),
+  {ok, Temperatur}.
+
+
+read_file() ->
+  file_read:readfile(<<"/sys/bus/w1/devices/28-000475762fff/w1_slave">>).
